228c8477091d696fd9fd695cb591d9c013bbb896
nakeliss <31477508+nakeliss@users.noreply.github.com>
2022-06-19 16:17:33 +0800

    Update test.R

--- test.R
+++ test.R
@@ -1,22 +1,23 @@
 library(stringr)
 library(eulerr)
+
 data <- read.csv("plot.csv",encoding="UTF-8")
 #print(data)
 #print(nrow(data))
 
 i=1
 v=1
-
 #print(data[i,1])
 #print(data[v,2])
 
+###读取与赋值测试
 print(sprintf(data[i,1]))
 print(c("SE" = data[v,2]))
 
-print(sprintf(data[i,1]) = data[v,2])
+print(sprintf(data[i,1]) = data[v,2])#ERROR
 
 
-###
+###循环
 a=c()
 append(a,data[i,1] <- data[v,2])
 print(a)
@@ -32,7 +33,7 @@ repeat{
 
 }
 
-###
+###参考
 fit <- euler(c("A" = 4, "B" = 6, "C" = 3, "D" = 2, "E" = 7, "F" = 3,
                "A&B" = 2, "A&F" = 2, "B&C" = 2, "B&D" = 1,
                "B&F" = 2, "C&D" = 1, "D&E" = 1, "E&F" = 1,
