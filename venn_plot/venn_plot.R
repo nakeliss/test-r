994931a571180aa48f17ace6e525e9b61f6127d2
nakeliss <745364784@qq.com>
2022-06-23 19:39:26 +0800

    1

--- venn_plot/venn_plot.R
+++ venn_plot/venn_plot.R
@@ -1,22 +1,137 @@
 library(VennDiagram)
 library(grid)
 
+library(ggvenn)
+library(dplyr)
+library(ggplot2)
+
+library(ggVennDiagram)
+
 #读取文件
 df <- read.csv("plot.csv",encoding="UTF-8")
+df2 <- read.csv("plot2.csv",encoding="UTF-8")
+
+
 set1 =list(df$Set1)
 set2 =list(df$Set2)
 set3 =list(df$Set3)
 set4 =list(df$Set4)
 set5 =list(df$Set5)
 
-print(set1)
-print(A)
+set21 =list(df2$Set1)
+set22 =list(df2$Set2)
+set23 =list(df2$Set3)
+set24 =list(df2$Set4)
+set25 =list(df2$Set5)
 
+
+print(set1)
+print(set2)
+print(set3)
+print(set4)
+print(set5)
+
+print(set21)
+print(set22)
+print(set23)
+print(set24)
+print(set25)
+
+
+###
+x2=list('Set 1'=set1,
+        'Set 2'=set2)
+
+x3=list('Set 1'=set1,
+       'Set 2'=set2,
+       'Set 3'=set3)
+x4=list('Set 1'=set1,
+        'Set 2'=set2,
+        'Set 3'=set3,
+        'Set 4'=set4)
+x5=list('Set 1'=set1,
+       'Set 2'=set2,
+       'Set 3'=set3,
+       'Set 4'=set4,
+       'Set 5'=set5)
+
+
+x22=list('Set 1'=set21,
+         'Set 2'=set22)
+
+x23=list('Set 1'=set21,
+       'Set 2'=set22,
+       'Set 3'=set23)
+x24=list('Set 1'=set21,
+         'Set 2'=set22,
+         'Set 3'=set23,
+         'Set 4'=set24)
+x25=list('Set 1'=set21,
+        'Set 2'=set22,
+        'Set 3'=set23,
+        'Set 4'=set24,
+        'Set 5'=set25)
+###
+
+ggVennDiagram(x22)#x
+
+###
+ggvenn(x2,
+       show_percentage = F,
+       stroke_color = "white",
+       fill_color = c("#ffb2b2","#b2e7cb"),
+       set_name_color = c("#ff0000","#4a9b83"))
+ggvenn(x3,
+       show_percentage = F,
+       stroke_color = "white",
+       fill_color = c("#ffb2b2","#b2e7cb","#b2d4ec"),
+       set_name_color = c("#ff0000","#4a9b83","#1d6295"))
+ggvenn(x4,
+       show_percentage = F,
+       stroke_color = "white",
+       fill_color = c("#ffb2b2","#b2e7cb","#b2d4ec","#d3c0e2"),
+       set_name_color = c("#ff0000","#4a9b83","#1d6295","#7030a2"))
+
+###5组数据不可用
+ggvenn(x5,
+       show_percentage = F,
+       stroke_color = "white",
+       fill_color = c("#ffb2b2","#b2e7cb","#b2d4ec","#d3c0e2"),
+       set_name_color = c("#ff0000","#4a9b83","#1d6295","#7030a2"))
+
+
+ggvenn(x22,
+       show_percentage = F,
+       stroke_color = "blue",
+       fill_color = c("#ffb2b2","#b2e7cb"),
+       set_name_color = c("#ff0000","#4a9b83"))
+ggvenn(x23,
+       show_percentage = F,
+       stroke_color = "blue",
+       fill_color = c("#ffb2b2","#b2e7cb","#b2d4ec"),
+       set_name_color = c("#ff0000","#4a9b83","#1d6295"))
+ggvenn(x24,
+       show_percentage = F,
+       stroke_color = "blue",
+       fill_color = c("#ffb2b2","#b2e7cb","#b2d4ec","#d3c0e2"),
+       set_name_color = c("#ff0000","#4a9b83","#1d6295","#7030a2"))
+
+###5组数据不可用
+ggvenn(x25,
+       show_percentage = F,
+       stroke_color = "blue",
+       fill_color = c("#ffb2b2","#b2e7cb","#b2d4ec","#d3c0e2"),
+       set_name_color = c("#ff0000","#4a9b83","#1d6295","#7030a2"))
+
+
+
+
+###
 #2
 venn.plot =venn.diagram(
-  x=list(set1, set2),
+  x=list(set21, set22),
   category.names = c("Set 1" , "Set 2 "),
-  filename = 'venn2.png',#保存路径
+  filename = './out/venn2.png',#保存路径
   col ="black",#指定图形的圆周边缘颜色  transparent 透明
   fill=c("red","blue"),#填充颜色
   alpha=0.5,#透明度
@@ -36,7 +151,7 @@ venn.plot =venn.diagram(
 venn.plot <- venn.diagram(#数据列表
   x = list(set1, set2, set3, set4, set5),
   category.names = c("Set 1" , "Set 2 " , "Set 3", "Set 4", "Set 5"),
-  filename = "Venn5.png",    #保存路径
+  filename = "./out/Venn5.png",    #保存路径
   col = "transparent",      #指定图形的圆周边缘颜色  transparent 透明          
   fill = c("blue", "green", "yellow", "grey50","pink"),  #填充颜色
   alpha = 0.50,     #透明度
@@ -58,7 +173,7 @@ venn.plot <- venn.diagram(#数据列表
 venn.diagram(
   x = list(set1, set2),
   category.names = c("Set 1" , "Set 2 "),
-  filename = 'venn2.png',
+  filename = './out/venn2.png',
   output=TRUE
 )
 
@@ -66,21 +181,21 @@ venn.diagram(
 venn.diagram(
   x = list(set1, set2, set3),
   category.names = c("Set 1" , "Set 2 " , "Set 3"),
-  filename = 'venn3.png',
+  filename = './out/venn3.png',
   output=TRUE
 )
 
 venn.diagram(
   x = list(set1, set2, set3, set4),
   category.names = c("Set 1" , "Set 2 " , "Set 3", "Set 4"),
-  filename = 'venn4.png',
+  filename = './out/venn4.png',
   output=TRUE
 )
 
 venn.diagram(
   x = list(set1, set2, set3, set4, set5),
   category.names = c("Set 1" , "Set 2 " , "Set 3", "Set 4", "Set 5"),
-  filename = 'venn5.png',
+  filename = './out/venn5.png',
   output=TRUE
 )
 
@@ -94,7 +209,7 @@ color <- brewer.pal(3, "Set3")
 venn.diagram(
   x = list(set1, set2, set3),
   category.names = c("Set 1" , "Set 2 " , "Set 3"),
-  filename = 'venn2.png',
+  filename = './out/venn2.png',
   output=TRUE,
   
   # 输出
@@ -131,7 +246,7 @@ library(RColorBrewer)
 p = venn.diagram(
   x = list(set1, set2, set3, set4, set5),
   category.names = c("Set 1" , "Set 2 " , "Set 3", "Set 4", "Set 5"),
-  # filename = 'venn.png',
+  # filename = './out/venn.png',
   filename = NULL,
   output=TRUE,
   
